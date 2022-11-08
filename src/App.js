41d909ed026401ed68e83d85da5865a0e6aa9479
Trommvain <trommvain@gmail.com>
2022-11-08 15:37:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const product = {
+    name: "Smartphone",
+    description:
+      "Get in touch with the future, enjoy expirience with the new smartphone.",
+    price: "100$",
+    amount: 10,
+  };
+
+  //Functional component
+
+  function ProductCard(props) {
+    return (
+      <div className="product-card">
+        <h1>{props.name || "No name"}</h1>
+        <p>{props.description || "No description available"}</p>
+        <p className="price">{props.price || "unknown price"}</p>
+        <p className={props.amount > 0 ? "available" : "not-available"}>
+          {props.amount > 0 ? "Available" : "Not available"}
+        </p>
+      </div>
+    );
+  }
+
+  //Class component
+
+  // class ProductCard extends React.Component {
+  //   render() {
+  //     const { name, description, price, amount } = this.props;
+  //     return (
+  //       <div className="product-card">
+  //         <h1>{name || "No name"}</h1>
+  //         <p>{description || "No description available"}</p>
+  //         <p className="price">{price || "unknown price"}</p>
+  //         <p className={amount > 0 ? "available" : "not-available"}>
+  //           {amount > 0 ? "Available" : "Not available"}
+  //         </p>
+  //       </div>
+  //     );
+  //   }
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductCard
+        name={product.name}
+        description={product.description}
+        price={product.price}
+        amount={product.amount}
+      />
     </div>
   );
 }
